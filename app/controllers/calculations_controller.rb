b0a59b8ca96b4afc20dc019ef33fae4ecbc77bde
dhheck <dheck@chicagobooth.edu>
2015-10-13 23:12:44 -0500

    DHH Omnicalc w/Ruby Loan Complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,13 +11,13 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.gsub(" ", "").length
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split.count
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.downcase.split.count(@special_word.downcase)
 
     # ================================================================================
     # Your code goes above.
@@ -38,7 +38,7 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = ((@apr / 100 / 12) * @principal) / (1 - (1 + (@apr / 100 / 12)) ** ( - (@years * 12)))
 
     # ================================================================================
     # Your code goes above.
@@ -60,12 +60,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = (@starting - @ending).abs
+    @minutes = ((@starting - @ending).abs) / 60
+    @hours   = ((@starting - @ending).abs) / 60 / 60
+    @days    = ((@starting - @ending).abs) / 60 / 60 / 24
+    @weeks   = ((@starting - @ending).abs) / 60 / 60 / 24 / 7
+    @years   = ((@starting - @ending).abs) / 60 / 60 / 24 / 7 / (365 / 7)
 
     # ================================================================================
     # Your code goes above.
